b2eb6fa1ebf40add9b906071b1eef0148f29652f
VageeshB <vageesh_bhasin@outlook.com>
2014-05-16 15:14:26 +0530

    Restructured main file

--- README.md
+++ README.md
@@ -1,6 +1,31 @@
 Selenium Automation Framework
 ============================
 
-* Run from command-line using 'ruby main.rb'
-* Provide data files (keep them in data folder)
-* Result reports can be found in /results/ folder
+An automation framework to perform web application testing using Selenium. It reads test scenarios and execution list from data file provided and performs relevant actions.
+
+## Usage ##
+
+### General Guidelines ###
+* Main file is 'main.rb'
+* Data files will be used from /data/ folder only
+* All result reports are created in new folder with start time as folder name in /reports/ folder
+
+### User interface ###
+* Run from command line using 'ruby main.rb' in root folder
+* Follow instructions
+
+### Command Line Interface ###
+
+Options:
+
+```
+-h or --help       : Show command line options
+-d [Data File]     : Execute testing with using data from [Data File] in /data/ folder
+-e [Email Address] : Email last test result report to [Email Address]
+-t                 : Create default data template in /data/ folder (NOTE: Cannot be clubbed with other options)
+```
+
+## Roadmap ##
+
+* Provide support for selenium grid usage
+* Provide support for object repositories in data file
--- main.rb
+++ main.rb
@@ -1,153 +1,17 @@
-require "./bin/config"
-class SAF
-  def interface
-    FWConfig.log_break
-    print "\nWould you like to create a test execution template?(y/N) "
-    if gets.chomp! =~ /y/i
-      Reader.template
-      print "\nTemplate created at [/data/default_template.xls].\n\nPlease edit it as per your needs and re-run the program.\n"
-    else
-      FWConfig.log_break
-      print "\nLooking for data(xls) files in [/data] folder: \n\n"
-      d = Dir.new(Dir.pwd.to_s + "/data")
-      d.each {|file| print "#{file}\n" if file =~ /(.xls)$/i}
-      print "\nPlease enter the data file [In /data/ folder]: "
-      filename = gets.chomp!
-      filename = filename.to_s + ".xls" if !(filename =~ /(.xls)/i)
-      execute(filename)
-    end
-    FWConfig.log_break
-  end
+require "./bin/main.rb"
 
-  def create_template
-    Reader.template
-    FWConfig.log_break
-    print "\nTemplate created at [/data/default_template.xls].\n\nPlease edit it as per your needs and re-run the program.\n"
-    FWConfig.log_break    
-  end
-
-   def email(address)
-
-    if address && address =~ /^.+@.+$/i
-      
-
-      FWConfig.log_break
-      print "Emailing latest test result report to: #{address}"
-      FWConfig.log_break
-
-      path = File.expand_path("./reports/", File.dirname(__FILE__))
-      last_report = Dir.glob(File.join(path, '*/*.html')).max { |a,b| File.ctime(a) <=> File.ctime(b) }
-
-      if last_report
-        print "\tZipping results\n"
-        zipfile_name = create_zip(last_report)
-        print "\tResults archived\n\tCreating email\n"
-        deliver_mail(address, zipfile_name)
-        print "\tEmail sent"
-        File.delete(zipfile_name)
-        FWConfig.log_break
-      else
-        print "No test reports found! Please execute atleast 1 test run!"
-        FWConfig.log_break
-      end
-
-    elsif !address
-      FWConfig.log_break
-      print "WARN: Email option given, but no email address found!"
-      FWConfig.log_break
-    else
-      FWConfig.log_break
-      print "WARN: Invalid email address!"
-      FWConfig.log_break
-    end
-  end
-
-  def self.execute(filename)
-    # Require neccessary modules
-    require "./bin/reader"
-    require "./bin/reporter"
-    require "./bin/executor"
-
-    if File.exists? "./data/" + filename.to_s
-      r = Reader.new(filename)
-      if r.status
-        exec = Executor.new(r.url, r.execution_hash, r.test_data)
-        FWConfig.log_break
-        print "Started Test Run at: #{Time.new.strftime("%H:%M:%S")}\n"
-        FWConfig.log_break
-        print "\nTest Report at: /reports/#{exec.execute}.html\n"
-        FWConfig.log_break
-        print "Completed Test Run at: #{Time.new.strftime("%H:%M:%S")}\n"
-        FWConfig.log_break
-      else
-        print "\nThank You\n"
-      end
-    else
-      print "\nFile could not be found! Exiting!\n"
-    end
-
-  end
-
-  private
-    def create_zip(filename)
-      require 'zip'
-      path = File.expand_path("./reports/", File.dirname(__FILE__))
-
-      resources = []
-      resources_path = File.expand_path("./reports/resources", File.dirname(__FILE__))
-      resources << File.join(resources_path, 'css/bootstrap.min.css')
-      resources << File.join(resources_path, 'js/bootstrap.min.js')
-      resources << File.join(resources_path, 'js/report.min.js')
-
-      report_file = filename.to_s
-
-      error_files = Dir.glob(File.join(path, "#{filename.split('/')[-2]}/*.png"))
-      archive_name = report_file.split('/').last.split('.').first
-      
-      zipfile_name = File.expand_path("./reports/", File.dirname(__FILE__)).to_s + "/report_archive.zip"
-      
-      Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
-        zipfile.mkdir("#{archive_name}")
-        zipfile.add("#{archive_name}/report.html", report_file)
-        error_files.each do |file|
-          zipfile.add("#{archive_name}/#{file.split('/').last.to_s}", file)
-        end
-        zipfile.mkdir('resources/css')
-        zipfile.mkdir('resources/js')
-        resources.each do |resource|
-          zipfile.add('resources/css/' + resource.split('/').last.to_s, resource) if resource.to_s =~ /css/i
-          zipfile.add('resources/js/' + resource.split('/').last.to_s, resource) if resource.to_s =~ /js/i
-        end
-      end
-      zipfile_name
-    end
-
-    def deliver_mail(receiver, filename)
-      require "mail"
-      Mail.defaults do
-        delivery_method :smtp, address: "smtp.gmail.com", user_name: 'vageesh6260@gmail.com', password: 'Samsung.s2', port: 587, authentication: 'plain', enable_starttls_auto: true
-      end
-      mail = Mail.new do
-        from    'selenium_automation_framework@reporter.io'
-        to      receiver
-        subject 'SAF Test Execution Result'
-        body    "Hi,\n\nThis is an auto-generated mail.\n\nPlease do not reply to this message.\n\nTest Execution Results have been attached.\n\nRegards,\nSAF Team"
-        add_file filename
-      end
-      mail.deliver
-    end
-end
-
-
-saf = SAF.new
+# Instantiate Framework class
+f = Framework.new
 
 # Main driver
 if ARGF.argv.empty?
   # No Options given, provide CLI
-  saf.interface
+  f.interface
 else
   # CL Options given, parse the options and execute
   args = ARGF.argv
+
+  # Help option
   if args.index('-h') || args.index('--help')
     FWConfig.log_break
     print "\nSelenium Automation Framework\n"
@@ -160,17 +24,24 @@ else
     exit
   end
 
+  # Store other options
   create_template = args.index('-t')
   data_file = args[args.index('-d') + 1] if args.index('-d')
   address = args[args.index('-e') + 1] if args.index('-e')
+
+  # '-t' option specified
   if create_template
-    saf.create_template
+    f.create_template
     exit
-  end  
-  if data_file
-    saf.execute(data_file)
   end
+
+  # '-d' option specified, read data file following '-d'
+  if data_file
+    f.execute(data_file)
+  end
+
+  # '-e' option specified, read email address following '-e'
   if address    
-    saf.email(address)
+    f.email(address)
   end
 end
